5dc0118cd073a60f240078184cb07e2018a42a8b
Reginaldo Ré <reginaldo@utfpr.edu.br>
2021-12-16 17:06:21 -0300

    It adds old functions of occurrences of species.

---
+++ R/ocurrences.R
@@ -0,0 +1,135 @@
+#' @export
+occurrences_to_shapefile <- function(o_file, sp_names, shp, proj){
+  sp_names <- sp_names %>%
+    unlist() %>%
+    to_snake_case()
+
+  sp_occurrences <- o_file %>%
+    vroom(show_col_types = F) %>%
+    clean_names() %>%
+    remove_empty(c("rows", "cols"))
+
+  sp_column_name <- sp_occurrences %>%
+    select_if(is.character) %>%
+    names() %>%
+    pluck(1)
+
+  sp_occurrences <- sp_occurrences %>%
+    rename(species = all_of(sp_column_name)) %>%
+    mutate(species = species %>% to_snake_case())
+
+  if (!is.na(sp_names) && !is.null(sp_names) && length(sp_names)>0)
+    sp_occurrences <-  sp_occurrences %>%
+    filter(species  %in% sp_names)
+
+  if (is.character(shp))
+    shp <- shp %>%
+    readOGR(verbose = F)
+
+  sp_occurrences$longitude <- sp_occurrences$longitude %>%
+    as.character() %>%
+    as.numeric()
+
+  sp_occurrences$latitude <- sp_occurrences$latitude %>%
+    as.character() %>%
+    as.numeric()
+
+  coordinates(sp_occurrences) <- ~longitude+latitude
+  if (is.character(proj)){
+    crs(sp_occurrences) <- CRS(proj)
+  } else {
+    crs(sp_occurrences) <- proj
+  }
+
+  sp_occurrences %>%
+    spTransform(crs(shp)) %>%
+    return()
+}
+
+#' @export
+occurrences_to_pa_shapefile <- function(shp_occ, shp_area, sp_names){
+  sp_names <- sp_names %>%
+    unlist() %>%
+    to_snake_case()
+
+  pa_matrix <- shp_area@data[, F]
+
+  for (spp in sp_names){
+    pa_matrix <- pa_matrix %>%
+      bind_cols(
+        shp_area %>%
+          over(shp_occ[shp_occ@data$species==spp, ])
+      )
+  }
+  names(pa_matrix) <- sp_names
+
+  pa_matrix[!is.na(pa_matrix)] <- 1
+  pa_matrix[is.na(pa_matrix)] <- 0
+
+  pa_matrix <- pa_matrix %>%
+    mutate_all(~ as.integer(.))
+
+  grid_pa_matrix <- shp_area
+
+  grid_pa_matrix@data <-  pa_matrix %>%
+    as.data.frame() %>%
+    set_rownames(shp_area@data %>% rownames())
+
+  return(grid_pa_matrix)
+}
+
+#' @export
+map_of_occurrences <- function(shp_occ, shp_area, title="", crs_subtitle=T){
+  map_tmp <- ggplot(
+    data =  fortify(shp_area),
+    aes(
+      x = long,
+      y = lat,
+      group=group
+    )
+  )
+
+  map_tmp <- map_tmp +
+    scale_x_continuous(labels = comma) +
+    scale_y_continuous(labels = comma) +
+    coord_equal()
+
+  if (title!=""){
+    map_tmp <- map_tmp +
+      labs(title=title)
+  }
+
+  if (crs_subtitle){
+    map_tmp <- map_tmp +
+      labs(subtitle = paste0(crs(shp_area)))
+  }
+
+  map_tmp <- map_tmp +
+    geom_polygon(colour = "black", fill=NA)
+
+  map_tmp <- map_tmp +
+    geom_point(data = fortify(shp_occ), aes(x = long, y = lat, group=species, color=species))
+
+  return(map_tmp)
+}
+
+#' @export
+map_of_pa <- function(shp_pa, shp_area, sp_names){
+  sp_names <- sp_names %>%
+    unlist() %>%
+    to_snake_case()
+
+  df_temp <- shp_pa@data %>%
+    rownames_to_column("id") %>%
+    pivot_longer(-id, names_to = "species", values_to="presence")
+
+  legend_title  <- ifelse(length(sp_names)==1, sp_names[[1]], "Presence/Absence")
+
+  ggplot(data = fortify(shp_area) %>% left_join(df_temp)) +
+    aes(x = long, y = lat, group = group) +
+    geom_polygon(aes(fill = as.factor(presence))) +
+    scale_fill_manual(legend_title, values=c("darkseagreen2", "tomato")) +
+    scale_x_continuous(labels = comma) +
+    scale_y_continuous(labels = comma) +
+    coord_equal()
+}
