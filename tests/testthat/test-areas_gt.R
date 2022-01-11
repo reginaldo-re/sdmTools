a3c98d82f6dfe61af97f37582d87e84151ce65c9
Reginaldo Ré <reginaldo@utfpr.edu.br>
2022-01-11 11:24:48 -0300

    It improves tests of the area_gt method.

--- tests/testthat/test-areas_gt.R
+++ tests/testthat/test-areas_gt.R
@@ -3,25 +3,51 @@ test_that("Trying to create an invalid objects SDM_area.", {
 })
 
 test_that("Removing a single area from a SpatialPolygons study area.", {
-  new_area <- SP %>%
-    sdm_area("Test area", "EPSG:6933", c(50000, 50000)) %>%
-    areas_gt(0.25, new_name = "Test area")
+  new_SPDF <- SP %>%
+    as("SpatialPolygonsDataFrame")
 
+  new_SPDF@data <- list(
+    area=c(
+      SP[1] %>% gArea(),
+      SP[2] %>% gArea(),
+      SP[3] %>% gArea()
+    )
+  ) %>%
+    as.data.frame()
+
+  new_area <- new_SPDF %>%
+    sdm_area("Removing a single area from a SpatialPolygons study area.", "EPSG:6933", c(50000, 50000)) %>%
+    areas_gt(0.25, new_name = "Removing a single area from a SpatialPolygons study area.")
+
+  expect_equal(new_area$study_area$area %>% sum() %>% round(2), 0.98)
   expect_equal(new_area$study_area %>% gArea() %>% round(2), 1.09)
-  expect_string(new_area$name, "Test area")
+  expect_string(new_area$name, "Removing a single area from a SpatialPolygons study area.")
 })
 
 test_that("Removing no areas from a SpatialPolygons study area.", {
-  new_area <- SP %>%
-    sdm_area("Test area", "EPSG:6933", c(50000, 50000)) %>%
+  new_SPDF <- SP %>%
+    as("SpatialPolygonsDataFrame")
+
+  new_SPDF@data <- list(
+    area=c(
+      SP[1] %>% gArea(),
+      SP[2] %>% gArea(),
+      SP[3] %>% gArea()
+    )
+  ) %>%
+    as.data.frame()
+
+  new_area <- new_SPDF %>%
+    sdm_area("Removing no areas from a SpatialPolygons study area.", "EPSG:6933", c(50000, 50000)) %>%
     areas_gt(0.1)
 
+  expect_equal(new_area$study_area$area %>% sum() %>% round(2), 1.23)
   expect_equal(new_area$study_area %>% gArea() %>% round(2), 1.34)
 })
 
 test_that("Removing all areas from a SpatialPolygons study area.", {
   new_area <- SP %>%
-    sdm_area("Test area", "EPSG:6933", c(50000, 50000)) %>%
+    sdm_area("Removing all areas from a SpatialPolygons study area.", "EPSG:6933", c(50000, 50000)) %>%
     areas_gt(20)
 
   expect_error(gArea(new_area$study_area))
@@ -30,7 +56,7 @@ test_that("Removing all areas from a SpatialPolygons study area.", {
 test_that("Removing no areas from study area using SpatialPolygonsDataframe.", {
   new_area <- SP %>%
     as("SpatialPolygonsDataFrame") %>%
-    sdm_area("Test area", "EPSG:6933", c(50000, 50000)) %>%
+    sdm_area("Removing no areas from study area using SpatialPolygonsDataframe.", "EPSG:6933", c(50000, 50000)) %>%
     areas_gt(0.1)
 
   expect_equal(new_area$study_area %>% gArea() %>% round(2), 1.34)
