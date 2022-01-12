95dcee77b8dfc01a7a7ef49138918ee9aad4a31d
Reginaldo Ré <reginaldo@utfpr.edu.br>
2022-01-12 18:00:04 -0300

    It removes sdm_area_composite class.

--- R/sdm_area_composite.R
+++
@@ -1,8 +0,0 @@
-sdm_area_composite <- function(a_path = NULL) {
-  UseMethod("sdm_area_composite", a_path)
-}
-
-character.sdm_area_composite <- function(a_path = NULL) {
-  checkmate::assert_string(a_path)
-
-}
