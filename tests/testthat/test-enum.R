e2c12a2d145b4687e4478b0d8e2f441da5dda713
Reginaldo Ré <reginaldo@utfpr.edu.br>
2022-01-21 11:31:17 -0300

    It adds a test case to test the enum creation.

--- tests/testthat/test-enum.R
+++ tests/testthat/test-enum.R
@@ -6,7 +6,12 @@ test_that("It creates an enum.", {
   )
 })
 
-test_that("It tries to create an invalid enum", {
+test_that("It tries to create an invalid enum.", {
   expect_error(create_enum(dummy, cell_id, x_centroid, y_centroid, dummy))
 })
 
+test_that("It creates ATTR_CONTROL_NAMES global constant.", {
+  expect_true(
+    ((ATTR_CONTROL_NAMES %>% enum_as_vector() == c("dummy", "cell_id", "x_centroid", "y_centroid")) %>% all())
+  )
+})
