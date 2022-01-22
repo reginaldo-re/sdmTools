1bf533184db77d6f2926b65d12f4bb45e0d30fbf
Reginaldo Ré <reginaldo@utfpr.edu.br>
2022-01-22 08:23:15 -0300

    It refactors creation and conversion of enumerations.

--- R/enum.R
+++ R/enum.R
@@ -19,8 +19,10 @@ create_enum <- function(...) {
 }
 
 enum_as_vector<- function(a_enum){
+  checkmate::check_class(a_enum, "enumeration")
   a_enum %>% unlist(use.names = F)
 }
 
 
 ATTR_CONTROL_NAMES <- create_enum(dummy, cell_id, x_centroid, y_centroid)
+RASTER_FORMATS_EXT <- create_enum(grd, asc, sdat, rst, nc, tif, envi, bil, img)
--- tests/testthat/test-enum.R
+++ tests/testthat/test-enum.R
@@ -10,8 +10,20 @@ test_that("It tries to create an invalid enum.", {
   expect_error(create_enum(dummy, cell_id, x_centroid, y_centroid, dummy))
 })
 
+test_that("It tries to convert to vector a non enumeration object.", {
+  expect_error(c("aaa") %>% enum_as_vector())
+})
+
+
 test_that("It creates ATTR_CONTROL_NAMES global constant.", {
   expect_true(
     ((ATTR_CONTROL_NAMES %>% enum_as_vector() == c("dummy", "cell_id", "x_centroid", "y_centroid")) %>% all())
   )
 })
+
+test_that("It creates RASTER_FORMATS_EXT global constant.", {
+  expect_true(
+    ((RASTER_FORMATS_EXT %>% enum_as_vector() == c("grd", "asc", "sdat", "rst", "nc", "tif", "envi", "bil", "img")) %>% all())
+  )
+})
+
