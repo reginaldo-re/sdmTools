0f4eb541f91449816b4ed01a5f970d74c2926586
Reginaldo Ré <reginaldo@utfpr.edu.br>
2022-01-26 23:17:34 -0300

    It imports some packages and change information about the package.

--- R/sdmTools-package.R
+++ R/sdmTools-package.R
@@ -8,7 +8,9 @@
 #' @importFrom methods is
 #' @importFrom methods slot
 #' @importFrom magrittr %>%
+#' @importFrom foreach %dopar%
 #' @importFrom stats setNames
+#' @importFrom rlang :=
 "_PACKAGE"
 
 ## usethis namespace: start
