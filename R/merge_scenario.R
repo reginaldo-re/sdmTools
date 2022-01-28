3665bf7bdd8808385eab8c1f4c3ff00dc954a6e3
Reginaldo Ré <reginaldo@utfpr.edu.br>
2022-01-28 19:11:27 -0300

    It improves the functionality of the merge_scenario method.

--- R/merge_scenario.R
+++ R/merge_scenario.R
@@ -9,13 +9,13 @@
 #' @export
 #'
 #' @examples
-merge_scenario <- function(an_area = NULL, to_merge_scenario = NULL, new_path = NULL, var_names = NULL){
+merge_scenario <- function(an_area = NULL, to_merge_scenario = NULL, var_names = NULL, new_name = NULL, new_path = NULL){
   UseMethod("merge_scenario", an_area)
 }
 
 
 #' @export
-merge_scenario.default <- function(an_area = NULL, to_merge_scenario = NULL, new_path = NULL, var_names = NULL) {
+merge_scenario.default <- function(an_area = NULL, to_merge_scenario = NULL, var_names = NULL, new_name = NULL, new_path = NULL) {
   warning("Nothing to do, an_area must be an SDM_area object.")
   an_area %>%
     return()
@@ -23,29 +23,77 @@ merge_scenario.default <- function(an_area = NULL, to_merge_scenario = NULL, new
 
 
 #' @export
-merge_scenario.SDM_area <- function(an_area = NULL, to_merge_scenario = NULL, new_path = NULL, var_names = NULL) {
+merge_scenario.SDM_area <- function(an_area = NULL, to_merge_scenario = NULL, var_names = NULL, new_name = NULL, new_path = NULL) {
   if (!an_area$gridded){
     an_area <- an_area %>%
       make_grid.SDM_area(var_names)
   }
 
   an_area %>%
-    .sp_merge_scenario(to_merge_scenario, new_path, var_names) %>%
+    .sp_merge_scenario(to_merge_scenario, var_names, new_name, new_path) %>%
     return()
 }
 
 
-.sp_merge_scenario <- function(an_area = NULL, to_merge_scenario = NULL, new_path = NULL, var_names = NULL) {
+.sp_merge_scenario <- function(an_area = NULL, to_merge_scenario = NULL, var_names = NULL, new_name = NULL, new_path = NULL) {
   checkmate::assert_class(an_area, "SDM_area")
   checkmate::assert_class(to_merge_scenario, "SDM_scenario")
-  checkmate::assert(
-    checkmate::check_string(new_path),
-    checkmate::check_directory_exists(new_path)
-  )
   checkmate::assert(
     checkmate::check_null(var_names),
     checkmate::check_list(var_names, types = c("character"), unique = T)
   )
+  checkmate::assert_string(new_name)
+  checkmate::assert(
+    checkmate::check_string(new_path),
+    checkmate::check_directory_exists(new_path)
+  )
+
+  .merge_scenario <- function(an_area = NULL, a_scenario = NULL, an_element = NULL, new_path = NULL, new_name = NULL){
+    atomic_scenario_list <- an_element %>%
+      discard(~ is.list(.))
+
+    nested_scenario <- an_element %>%
+      keep(~ is.list(.))
+
+    if (atomic_scenario_list %>% length() > 0){
+      var_names <- atomic_scenario_list %>%
+        unlist() %>%
+        unname() %>%
+        fs::path_file() %>%
+        unique() %>%
+        fs::path_ext_remove() %>%
+        as.list()
+
+      foreach::foreach(
+        atomic_scenario = atomic_scenario_list %>% unlist() %>% unname() %>% fs::path_dir() %>% unique(),
+        .packages=c("sdmTools", "magrittr", "stringr", "fs", "dplyr")) %dopar% {
+      #for (atomic_scenario in atomic_scenario_list %>% unlist() %>% unname() %>% fs::path_dir() %>% unique()){
+        tmp_area <- an_area %>%
+          merge_area(
+            to_merge_area = atomic_scenario,
+            new_name = atomic_scenario %>% stringr::str_remove(paste0(a_scenario$path, "/", a_scenario$name, "/")) ,
+            var_names = var_names)
+
+        tmp_area$study_area@data <- tmp_area$study_area@data  %>%
+          dplyr::select(ATTR_CONTROL_NAMES %>% enum_as_vector() %>% dplyr::any_of()) %>%
+          dplyr::bind_cols(
+            tmp_area$study_area@data  %>%
+                      dplyr::select(-(an_area$study_area@data %>% names()))
+          )
+
+        tmp_area %>%
+          save_gpkg(
+            file_path = new_path %>% fs::path(new_name, tmp_area$name) %>% fs::path_dir(),
+            file_name = tmp_area$name %>% fs::path_file()
+            )
+        rm(tmp_area)
+        gc()
+      }
+    }
+
+    nested_scenario %>%
+      map(~ .merge_scenario(an_area, a_scenario, ., new_path, new_name))
+  }
 
   chk <- Sys.getenv("_R_CHECK_LIMIT_CORES_", "")
   if (nzchar(chk) && chk == "TRUE") {
@@ -60,64 +108,13 @@ merge_scenario.SDM_area <- function(an_area = NULL, to_merge_scenario = NULL, ne
     parallel::makeCluster() %T>%
     doParallel::registerDoParallel()
 
-  raster_list <- to_merge_scenario %>%
-    .flatten_scenario()
-
-
-  result <- foreach::foreach(a_raster = raster_list %>% names() %>% unique(), .packages=c("magrittr", "janitor", "fs", "dplyr")) %dopar% {
-    aaa <- raster_list %>% purrr::keep(names(.) == a_raster)
-    print(aaa)
-
-    # grid_variaveis_futuro <- shape_grid_estudo %>%
-    #   merge_area(pasta, nome_variaveis_futuro, new_name = T)
-    # grid_variaveis_futuro$name <- pasta %>%
-    #   fs::path_file() %>%
-    #   make_clean_names()
-    #
-    # grid_variaveis_futuro$study_area@data <- grid_variaveis_futuro$study_area@data %>%
-    #   mutate_at(nome_variaveis_futuro_x10 %>% unlist(), list(~./10))
-    #
-    # grid_variaveis_futuro %>%
-    #   save_gpkg(file_path = here::here("output_data"))
-    #
-    # rm(grid_variaveis_futuro)
-    # gc()
-  }
+  .merge_scenario(an_area, a_scenario, a_scenario$content, new_path, new_name)
+  # grid_variaveis_futuro$study_area@data <- grid_variaveis_futuro$study_area@data %>%
+  #   mutate_at(nome_variaveis_futuro_x10 %>% unlist(), list(~./10))
 
   parallel::stopCluster(cl)
 }
 
-.flatten_scenario <- function(a_scenario) {
-  get_raster_list <- function(an_element) {
-    atomic_elements <- an_element %>%
-      purrr::keep(~ !is.list(.))
 
-    nested_elements <- an_element %>%
-      purrr::keep(~ is.list(.))
 
-    raster_list <- NULL
-    if (nested_elements %>% length() > 0){
-      raster_list <- nested_elements %>%
-        purrr::flatten() %>%
-        get_raster_list()
 
-      raster_list <- nested_elements %>%
-        names() %>%
-        fs::path(raster_list)
-    }
-    raster_list <- atomic_elements %>%
-      purrr::map2(atomic_elements %>% names(), ~ .y %>% fs::path(.x)) %>%
-      purrr::flatten_chr() %>%
-      append(raster_list)
-
-    raster_list %>%
-      magrittr::set_names(raster_list %>% purrr::map(~ fs::path_dir(.)) %>% stringr::str_replace_all("/", ".")) %>%
-      return()
-  }
-  raster_list <- a_scenario$content %>% get_raster_list()
-  raster_list <- a_scenario$path %>%
-    fs::path(raster_list) %>%
-    as.list() %>%
-    magrittr::set_names(raster_list %>% names()) %>%
-    return()
-}
